e721b958315530faca0768acbe67993dc23dd6e9
oji <sifauzi@gmail.com>
2018-12-27 11:01:09 +0700

    Update dependencies Fontsawsome

--- src/Components/ListItem.js
+++ src/Components/ListItem.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 class ListItem extends Component {
 	deleteHandler(e){		
